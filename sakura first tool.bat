693061606747f6e346e773d9340cdfbfc65612ad
officeel_sakura <sraiin901@gmail.com>
2025-08-01 22:30:29 +0200

    promo tool

---
+++ sakura first tool.bat
@@ -0,0 +1,147 @@
+@echo off
+POWERSHELL.EXE -Command "Add-Type -AssemblyName System.Windows.Forms; [void] [System.Windows.Forms.Messagebox]::Show( 'Hello Welcome to sakura!', 'Welcome', 'OK', 'Warning' )"
+cls 
+MODE 150,15
+chcp 65001 >nul
+title tiktok officeel_sakura
+color D
+:officeel_sakura
+echo                [38;2;255;182;193m███╗   ███╗██╗   ██╗██╗  ████████╗██╗   ████████╗ ██████╗  ██████╗ ██╗     
+echo                [38;2;255;192;203m████╗ ████║██║   ██║██║  ╚══██╔══╝██║   ╚══██╔══╝██╔═══██╗██╔═══██╗██║     
+echo                [38;2;255;182;193m██╔████╔██║██║   ██║██║     ██║   ██║█████╗██║   ██║   ██║██║   ██║██║     
+echo                [38;2;255;174;185m██║╚██╔╝██║██║   ██║██║     ██║   ██║╚════╝██║   ██║   ██║██║   ██║██║     
+echo                [38;2;255;160;175m██║ ╚═╝ ██║╚██████╔╝███████╗██║   ██║      ██║   ╚██████╔╝╚██████╔╝███████╗
+echo                [38;2;255;240;245m╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝   ╚═╝      ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝
+echo.
+echo                                     [97m┌────────────────────────────┐
+echo                                     │ 1.[officeel_sakura     [97m│
+echo                                     │                            │
+echo                                     │ 2.credits                 │
+echo                                     └────────────────────────────┘
+
+for /f "tokens=2 delims==" %%u in ('echo %USERNAME%') do set "username=%%u"
+
+<nul set /p "=┌─── (@%username%) "
+echo.
+<nul set /p "=└─# "
+set /p choice=
+
+
+if %choice%==1 goto github
+if %choice%==2 goto credits
+
+
+pause
+goto github sakura
+
+:github
+start https://github.com/tiktokofficeelsakura/tiktok-officeel_sakura
+pause
+goto https://github.com/tiktokofficeelsakura/tiktok-officeel_sakura
+
+:credits
+cls
+color f
+cls 
+echo.
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo.
+echo [95m┌─────────────────────────────────────┐
+echo    [97mMade by [Officeel_sakura
+echo    [97mMade For [Officeel_sakura [97mPurposes Only!
+echo [95m└─────────────────────────────────────┘
+echo [97m
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+timeout 1 /NOBREAK >nul
+cls 
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo.
+echo [95m┌─────────────────────────────────────┐
+echo    [97mMade by [officeel_sakura
+echo    [97mMade For [officeel_sakura[97mPurposes Only!
+echo [95m└─────────────────────────────────────┘
+echo [97m
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+timeout 1 /NOBREAK >nul
+cls 
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo.
+echo [95m┌─────────────────────────────────────┐
+echo    [97mMade by [officeel_sakura
+echo    [97mMade For [officeel_sakura [97mPurposes Only!
+echo [95m└─────────────────────────────────────┘
+echo [97m
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+timeout 1 /NOBREAK >nul
+cls
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo.
+echo [95m┌─────────────────────────────────────┐
+echo    [97mMade by [officeel_sakura
+echo    [97mMade For [officeel_sakura [97mPurposes Only!
+echo [95m└─────────────────────────────────────┘
+echo [97m
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+timeout 1 /NOBREAK >nul
+cls
+echo.
+echo [95m┌─────────────────────────────────────┐
+echo    [97mMade by [officeel_sakura
+echo    [97mMade For [officeel_sakura[97mPurposes Only!
+echo [95m└─────────────────────────────────────┘
+echo [97m
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+timeout 1 /NOBREAK >nul
+cls
+echo.
+echo [95m┌─────────────────────────────────────┐
+echo    [97mMade by [sakura
+echo    [97mMade For [92mEducational [97mPurposes Only!
+echo [95m└─────────────────────────────────────┘
+echo [97m
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+cls 
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+timeout 1 /NOBREAK >nul
+cls 
+timeout 1 /NOBREAK >nul
+cls
+echo Thank you for using my multitool!
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+timeout 1 /NOBREAK >nul
+cls
+echo.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+timeout 1 /NOBREAK >nul
+cls
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+timeout 1 /NOBREAK >nul
+cls
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+timeout 1 /NOBREAK >nul
+cls
+timeout 1 /NOBREAK >nul
+goto officeel_sakura
+                                                    
